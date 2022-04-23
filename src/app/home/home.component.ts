e1c1b5db4b00a71caa510dc4b75a68f5fc472cd2
forsakenkimi <kckcheng@ucdavis.edu>
2022-04-23 15:32:27 -0700

    add home component subscription

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../core/services/movie.service';
+import { MovieCard } from '../shared/models/MovieCard';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  movies!:MovieCard[];
+  constructor(private movieService: MovieService) { }
 
   ngOnInit(): void {
+    console.log("Inside the Home Component inti method");
+    this.movieService.getTopGorssingMovies().subscribe(m => {this.movies = m; 
+      console.log(this.movies);
+    });
   }
 
 }
