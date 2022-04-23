ef6263371b07b42f14b1557477cb98095d8c10d1
forsakenkimi <kckcheng@ucdavis.edu>
2022-04-23 13:49:14 -0700

    first commit

---
+++ src/app/movies/movies.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+
+
+
+@NgModule({
+  declarations: [
+    MovieDetailsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MoviesModule { }
