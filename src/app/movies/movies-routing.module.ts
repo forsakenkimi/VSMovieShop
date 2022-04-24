e000a4df2ee89ee7cc3a13d77208dfe8672dbba7
forsakenkimi <kckcheng@ucdavis.edu>
2022-04-24 00:36:22 -0700

    added lazy module

---
+++ src/app/movies/movies-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CastDetailsComponent } from './cast-details/cast-details.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MoviesComponent } from './movies.component';
+
+const routes: Routes = [
+  {
+    path:'', component:MoviesComponent,
+    children:[
+      {path:':id', component: MovieDetailsComponent},
+      {path:'cast/:id', component:CastDetailsComponent}
+    ]
+  }
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MoviesRoutingModule { }
--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
+import { MoviesRoutingModule } from './movies-routing.module';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { CastDetailsComponent } from './cast-details/cast-details.component';
+import { MoviesComponent } from './movies.component';
 
 
 @NgModule({
   declarations: [
-    MovieDetailsComponent
+    MovieDetailsComponent,
+    CastDetailsComponent,
+    MoviesComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MoviesRoutingModule
   ]
 })
 export class MoviesModule { }
